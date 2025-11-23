552d671abf3c4399fa988f01f2e899f152b080df
neros29 <sorenmile29@gmail.com>
2025-11-22 23:34:46 -0700

    Hugely optimiezed the rendering using a singe & signe

--- rand.cpp
+++ rand.cpp
@@ -1,5 +1,24 @@
 #include <unistd.h>
+#include <string>
+#include <iostream>
+#include <sstream>
+using namespace std;
+
 int main(){
-    char a = 'a';
-    write(1, &a, 1);
+    char ch[8]{"😀"};
+    string ESC {"\x1B"};
+    ostringstream oss;
+    ostringstream _fg;
+    ostringstream _bg;
+    _fg << ESC << "[38;2;"<< 255 << ";" << 255 << ";"<< 255 <<"m";
+    _bg << ESC << "[48;2;"<< 255 << ";" << 255 << ";" << 255 << "m";
+    oss << _bg.str() << _fg.str() << ch << ESC << "[0m";
+    string ansii = oss.str();
+    char ch[ansii.size()];
+    int index {0};
+    for(char i:ansii){
+        ch[index] = i;
+        index++;
+    }
+    write(1, ch, ansii.size());
 }
--- test.cpp
+++ test.cpp
@@ -9,54 +9,52 @@
 
 using namespace std;
 
-// void set_raw() {
-//     struct termios term;
-//     tcgetattr(0, &term);
-//     term.c_lflag &= ~(ECHO | ICANON);
-//     tcsetattr(0, TCSANOW, &term);
-// }
+void set_raw() {
+    struct termios term;
+    tcgetattr(0, &term);
+    term.c_lflag &= ~(ECHO | ICANON);
+    tcsetattr(0, TCSANOW, &term);
+}
 
-// void restore() {
-//     struct termios term;
-//     tcgetattr(0, &term);
-//     term.c_lflag = (ECHO | ICANON);
-//     tcsetattr(0, TCSANOW, &term);
-// }
-// char get_char(){
-// 	char ch {};
-// 	if (read(0, &ch, 8) == 1){ // Do i need to uses read or is there a better way 
-// 		return ch;
-// 	}
-// 	return NULL;
+void restore() {
+    struct termios term;
+    tcgetattr(0, &term);
+    term.c_lflag = (ECHO | ICANON);
+    tcsetattr(0, TCSANOW, &term);
+}
+char get_char(){
+	char ch {};
+	if (read(0, &ch, 8) == 1){ // Do i need to uses read or is there a better way 
+		return ch;
+	}
+	return NULL;
 
-// }
+}
 
 int main() {
-    // set_raw();
-    // ofstream stdout("/dev/stdout");
-    // struct winsize ws = {(struct winsize){0,0,0,0}};
-    // ioctl(STDIN_FILENO, TIOCGWINSZ, &ws);
-    // cout << "\x1B[2J";
-    // cout << "\x1b[H";
-    // cout << "Hello terminal" <<  ws.ws_row << " " << ws.ws_col << endl;
-    // int index {0};
-    // while (true){
-	// 	char ch = get_char();
-	// 	if (ch == NULL) {
-	// 		continue;
-	// 	}
-	// 	if (ch == 'q'){
-	// 		break;
-	// 	}
-    //     cout << ch;
-    //     cout.flush(); // is there a better way then flushing every loop;
-    //     ++index;
-	// }
-	// cout << endl;
-    // restore();
+    set_raw();
+    ofstream stdout("/dev/stdout");
+    struct winsize ws = {(struct winsize){0,0,0,0}};
+    ioctl(STDIN_FILENO, TIOCGWINSZ, &ws);
+    cout << "\x1B[2J";
+    cout << "\x1b[H";
+    cout << "Hello terminal" <<  ws.ws_row << " " << ws.ws_col << endl;
+    int index {0};
+    while (true){
+		char ch = get_char();
+		if (ch == NULL) {
+			continue;
+		}
+		if (ch == 'q'){
+			break;
+		}
+        cout << ch;
+        cout.flush(); // is there a better way then flushing every loop;
+        ++index;
+	}
+	cout << endl;
+    restore();
 
-    int* arr{new int[2]};
-    arr[0] = 5;
-    cout << *arr << endl;
-    // return 0;
+   
+    return 0;
 }
