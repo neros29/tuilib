26636370ca91d86dd621dc83fa2572f51ddcaae9
neros29 <sorenmile29@gmail.com>
2026-01-11 18:31:57 -0700

    save 06:31

--- src/test.cpp
+++ src/test.cpp
@@ -16,34 +16,21 @@ int main(){
     array<int, 3> WHITE {255, 255, 255};
     array<int, 3> BLUE {0, 0, 255};
 
-
     auto &surf = screen.append({40, 10}, offset);
     auto &surf2 = screen.append({40, 10}, offset2);
-    // int of[2] {0, 0};
-    // int sz[2] {5, 5};
-    // for (int i = 0; i < 255; i ++){
-    //     auto &s = screen.append(sz, of);
-    //     s.fill_bg(i, i, i);
-    //     of[0] = i;
-    //
-    // }
-    //
-    //
+
     surf.fill_bg(210, 210, 210);
     surf.fill_fg(0, 0, 0);
     surf2.fill_bg(0, 255, 0);
 
     string str = "Hello world";
     Label lab(surf, str, {1, 1});
-    
-    // Surface sub({11, 1}, " ", 0, {0, 1});
-    // sub.fill_bg(255, 0, 0);
-    // surf.blit(sub);
 
     int color = 1;
     system("clear");
     cout << "\x1b[3J";
     int vec[2] {1, 1};
+
     unsigned long count = 0;
     time_t start = time(nullptr);
     time_t secounds = 10;
@@ -84,7 +71,6 @@ int main(){
         }
         else if (ch == 'l'){
             offset[0] ++;
-
         }
         else if (ch == '2'){
             surf.set_z(2);
